57e64cccb2acb9b2d345d84797ed1653b2d8d242
valeriy-capt <cherepanov.valeriy@gmail.com>
2017-06-20 20:48:34 +0300

    step three

--- dist/js/main.js
+++ dist/js/main.js
@@ -429,7 +429,7 @@ this.length++)}return this};(function(d,c,h){for(var i=0;i<c.length;i++)h(d,c[i]
   });
 }));
 
-(function($){
+var initObj = (function($){
 
     var init = function init() {
         customSelect();
@@ -460,5 +460,65 @@ this.length++)}return this};(function(d,c,h){for(var i=0;i<c.length;i++)h(d,c[i]
         .slicknav({label: ''});
     };
 
-    init();
+    var checkInput = function() {
+        $("[type='text'],[type='email']").each(function(){
+            if (this.value !== "") 
+                $(this).addClass('input-active');
+            else 
+                $(this).removeClass('input-active');
+        });
+    };
+
+    var onScroll = function(event){
+        var scrollPos = $(document).scrollTop();
+        $('#menu a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                $('#menu ul li a').removeClass("active");
+                currLink.addClass("active");
+            }
+            else{
+                currLink.removeClass("active");
+            }
+        });
+    }
+
+
+    return {
+        init: init,
+        onScroll: onScroll,
+        checkInput: checkInput
+    }
+
 })(jQuery);
+
+$(document).ready(function(){
+    $(document).on("scroll", initObj.onScroll);
+    $(document).on("change", initObj.checkInput);
+
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            indent = 10,
+            menu = target,
+            navHeight = $('.nav').height();
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top-navHeight-indent
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", initObj.onScroll);
+        });
+    });
+
+    initObj.init();
+    initObj.checkInput();
+});
